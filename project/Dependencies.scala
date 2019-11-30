1041b370cda083ef7a9912f5d870fc9dbcbc1ecd
Fernando Brule <fernandobrule@gmail.com>
2019-11-30 07:07:47 -0300

    incrementa versiones

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,6 @@ object Dependencies {
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.8" % "test"
   //
   lazy val fragnosticConf = "com.fragnostic" % "fragnostic-conf_2.13" % "0.1.9"
-  lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.10"
+  lazy val fragnosticSupport = "com.fragnostic" % "fragnostic-support_2.13" % "0.1.11"
 
 }
