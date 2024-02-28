34d44c60112ecef59473d6b819bc87ab33e1fa78
Fernando Brûlé <fernandobrule@gmail.com>
2024-02-27 22:55:34 -0300

    upd dependencies to final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,10 @@ import sbt._
 
 object Dependencies {
 
-  lazy val fragnosticI18nApi       = "com.fragnostic"  % "fragnostic-i18n-api_2.13" % "0.1.4-SNAPSHOT"
-  lazy val fragnosticSupport       = "com.fragnostic"  % "fragnostic-support_2.13"  % "0.1.19-SNAPSHOT"
+  lazy val fragnosticI18nApi       = "com.fragnostic"  % "fragnostic-i18n-api_2.13" % "0.1.4"
+  lazy val fragnosticSupport       = "com.fragnostic"  % "fragnostic-support_2.13"  % "0.1.19"
 
-  lazy val logbackClassic             = "ch.qos.logback"             % "logback-classic"                     % "1.3.0-alpha16"
-  //lazy val slf4jApi                = "org.slf4j"       % "slf4j-api"                % "2.0.0-alpha5"
+  lazy val logbackClassic          = "ch.qos.logback"  % "logback-classic"          % "1.3.0-alpha16"
   lazy val scalatestFunSpec        = "org.scalatest"   % "scalatest-funspec_2.13"   % "3.3.0-SNAP3" % Test
 
 }
